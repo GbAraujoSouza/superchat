a7c22d5323aa6e2061e8be2373b54a65bdfae329
GbAraujoSouza <gabriel.souzaaraujo109@gmail.com>
2024-06-24 18:30:27 -0300

    feat: init config

---
+++ backend2/src/index.ts
@@ -0,0 +1,43 @@
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected: " + socket.id);
+
+  socket.on("private message", async ({ senderid, receiverId, content }) => {
+    try {
+      // salvar mensagem
+      userService.create
+    } catch (error) {
+      
+    }
+  })
+
+  socket.on("send_message", (data) => {
+
+    socket.to(data.room).emit("receive_message", );
+  });
+
+  socket.on("join_room", (room) => {
+    socket.join(room);
+  })
+});
+
+server.listen(3001, () => {
+  console.log("Server is running at http://localhost:3001");
+});
