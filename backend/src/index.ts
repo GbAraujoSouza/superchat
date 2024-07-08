5ff0296bb011f07fbb2783d7a28d27d94550c640
GbAraujoSouza <gabriel.souzaaraujo109@gmail.com>
2024-07-08 10:24:03 -0300

    bigfix: typescript config file missing

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,7 +9,7 @@ import userRouter from "./routes/user.routes"
 
 const app = express();
 
-app.use(express.json());
+// app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // app.use(cors());
